c635410d1809c521e925ced93a51503d3b57c9d4
matthewbelevski <68624883+matthewbelevski@users.noreply.github.com>
2020-12-07 09:44:27 +1100

    Making progress with the individual plots

--- static/js/app v2.js
+++ static/js/app v2.js
@@ -15,16 +15,25 @@ var selector = d3.select("#selDataset");
     });
 }
 
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
 
 function optionChanged() {
 
   d3.json("/data/samples.json").then((importedData) => {
-    // console.log(importedData);
+     //console.log(importedData);
     
+
   var data = importedData.samples;
   var mdata = importedData.metadata;
 
     //console.log(importedData);
+  //var test = unpack(importedData.samples.sample_values);
+  
+  //console.log(test);
 
   var dropdownMenu = d3.select("#selDataset");
 
@@ -38,51 +47,68 @@ function optionChanged() {
 
   //console.log(data);
   //console.log(mdata);
-  //console.log(filteredData);
+  console.log(filteredData);
   //console.log(filteredMetadata);
 
   //console.log(filteredData);
   // Initialize an empty array for the country's data
 
-  // var test = filteredData.sample_values;
-
   // console.log(test)
 
- var sortedSamplevalues = filteredData.sort(function(a, b) {
+
+ //console.log(test_sample_values);
+
+ //console.log(x);
+ var sampleValues = filteredData[0].sample_values;
+ var otu_ids = filteredData[0].otu_ids;
+ var otu_labels = filteredData[0].otu_labels;
+ 
+
+ var sortedSamplevalues = sampleValues.sort(function(a, b) {
     return parseFloat(b) - parseFloat(a);
   });
 
+  var test = sampleValues.sort(function(a, b) {
+    return parseFloat(b) - parseFloat(a);
+  });
+// var indices = []
 
 
-  //console.log(sortedSamplevalues);
+console.log(sortedSamplevalues);
+
+//var test = sortedSamplevalues.map(data => data.sample_values);
+
+//console.log(test);
 
 // Slice the first 10 objects for plotting
 slicedData = sortedSamplevalues.slice(0, 10);
 
-console.log(slicedData);
+//console.log(slicedData);
 
 // Reverse the array to accommodate Plotly's defaults
 reversedData = slicedData.reverse();
 
+console.log(reversedData);
 
-d3.select("sample-metadata").selectAll("before")
-.selectAll("li")
-.data(filteredMetadata)
-.enter()
-.append("li")
-.text(function(d) {
-  return d;
+// d3.select(".sample-metadata").selectAll("div")
+// .append("table")
+// .data(filteredMetadata)
+// .enter()
+// .html(function(d) {
+//   return `<li>${d.id}</li><li>${d.ethnicity}</li><li>${d.gender}</li><li>${d.location}</li><li>${d.wfreq}</li><li>${d.bbtype}</li>`;
 
 
-  });
-
+//   });
 
 // Trace1 for the Greek Data
+var yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse()
+var txt = otu_labels.slice(0,10).map(id => `OTU ${id}`).reverse()
+
 var trace1 = {
-x: reversedData.map(object => object.sample_values),
-y: reversedData.map(object => object.otu_ids),
-text: reversedData.map(object => object.out_labels),
-name: "Greek",
+x: sampleValues.slice(0,10).reverse(),
+y: yticks,
+text: txt,
+name: "IDs",
 type: "bar",
 orientation: "h"
 };
@@ -92,7 +118,7 @@ var data1 = [trace1];
 
 // Apply the group bar mode to the layout
 var layout = {
-title: "Greek gods search results",
+title: "Observations",
 margin: {
   l: 100,
   r: 100,
@@ -103,9 +129,9 @@ margin: {
 
 Plotly.newPlot("bar", data1, layout)
 
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-}
+// function updatePlotly(newdata) {
+//   Plotly.restyle("pie", "values", [newdata]);
+// }
 
 }
 
